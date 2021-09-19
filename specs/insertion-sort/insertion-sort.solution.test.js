dc6f3605f6a3fcaf5729f990ce9510a1eca3e5d2
moonorange <monoma632@gmail.com>
2021-09-19 17:54:50 +0900

    add: insertion sort

--- specs/insertion-sort/insertion-sort.solution.test.js
+++ specs/insertion-sort/insertion-sort.solution.test.js
@@ -27,7 +27,7 @@ function insertionSort(nums) {
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test("insertion sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
